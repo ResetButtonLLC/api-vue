4d7dde057dd51c11c63c300ddbedbebd3e59eb68
optim1zer <o.kniaziev@promodo.com>
2022-02-24 08:19:14 +0200

    + update front vendors + ignore dist assets

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Request;
-use App\Http\Controllers\API\UserController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    return Auth::user();
 });
