105a52009f516773adbd9a574c6cdfb514108f2e
Reset <counterfate@gmail.com>
2022-04-29 08:58:46 +0300

    - laravel update 9 & PHP 8.1

--- app/Http/Controllers/API/ClientController.php
+++ app/Http/Controllers/API/ClientController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\ClientCreateRequest;
 use App\Http\Resources\ClientResource;
 use App\Models\Client;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -13,8 +14,10 @@ class ClientController extends Controller
 {
     public function index(Request $request)
     {
+        //auth()->user()->load('clients')->clients
+        //todo решить с ответом
         return ClientResource::collection(
-            Client::where('user_id', Auth::id())->get()
+           []
         );
     }
 
