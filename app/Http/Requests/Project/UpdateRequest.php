f0d9f1ca77aeb1dd7332fdea9e9221c02e219258
Reset <counterfate@gmail.com>
2022-05-11 16:36:13 +0300

    - auth on updating user in project

--- app/Http/Requests/Project/UpdateRequest.php
+++ app/Http/Requests/Project/UpdateRequest.php
@@ -8,6 +8,16 @@ use Illuminate\Validation\Rule;
 class UpdateRequest extends ApiRequest
 {
 
+    public function authorize() : bool
+    {
+        //Обновлять пользователей из проекта может только администратор, поэтому если есть массив с добавлением пользователей, то проверяем права
+        if (request()->get('users',[])) {
+            return auth()->user()->isAdmin();
+        } else {
+            return true;
+        }
+    }
+
     public function rules()
     {
         return [
