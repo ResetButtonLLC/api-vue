43c1d5a0ce37daf6ded4d96cf3797cccf78a3fff
Reset <counterfate@gmail.com>
2022-05-12 15:12:07 +0300

    - clean up

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,9 +4,7 @@ namespace App\Providers;
 
 use App\Models\Profile;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Gate;
-use App\Policies\ProjectPolicy;
 use App\Models\User;
 use App\Models\Project;
 
